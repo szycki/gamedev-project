5178453658bfa163f9053f59e9c5abf2cb9a496a
szycki <szycki@edu.cdv.pl>
2023-03-30 20:02:09 +0200

    add player, add ball, add ball bouncing

---
+++ Assets/MoveSquare.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSquare : MonoBehaviour
+{
+    public Rigidbody2D rigidbody2D;
+    public float speed;
+    public KeyCode upKey;
+    public KeyCode downKey;
+    void Start()
+    {
+        speed = 2;
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.gravityScale = 0;
+    }
+
+    void Update()
+    { 
+        if(Input.GetKey(upKey) && transform.position.y < 5)
+        {
+            rigidbody2D.velocity = Vector2.up * speed;
+        }
+        else if (Input.GetKey(downKey) && transform.position.y > -5)
+        {
+            rigidbody2D.velocity = Vector2.down * speed;
+        }
+        else
+        {
+            rigidbody2D.velocity = Vector2.zero;
+        }
+    }
+}
